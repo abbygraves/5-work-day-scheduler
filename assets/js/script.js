f514c2c3bb0a6dd7bc28fdeb5c6a5614b7d80937
abbygraves <abbygraves14@gmail.com>
2023-01-18 17:30:31 -0600

    cleaned up code, removed/commented out unnecessary code, added explanations comments to explain blocks of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var currentTime = moment().hours();
 var currentDay = document.querySelector("#currentDay");
 var textArea = document.querySelector(".text-field");
 var currentDate = new Date();
-  
+
 
 function showDate() {
   var rightNow = moment().format("dddd, MMMM Do");
@@ -12,13 +12,13 @@ function showDate() {
 };
 
 function colorCoding() {
-  for (let i = 8; i <=17; i++) {
+  for (let i = 8; i <= 17; i++) {
     if (i < currentTime) {
       $("#" + i).addClass("past");
-    } 
+    }
     else if (i === currentTime) {
-      $("#" + i ).addClass("present"); 
-    } 
+      $("#" + i).addClass("present");
+    }
     else if (i > currentTime) {
       $("#" + i).addClass("future");
     }
@@ -26,7 +26,7 @@ function colorCoding() {
 };
 
 
-$("textarea").on("click", function () {
+/* $("textarea").on("click", function () {
   console.log("text clicked!!!!!")
   var text = $(this)
     .text()
@@ -35,42 +35,43 @@ $("textarea").on("click", function () {
   var textInput = $("<textarea>")
     .val(text);
 
-  $(this).replaceWith(textInput);
-  
-  textInput.trigger("focus");
-});
+  // $(this).replaceWith(textInput);
 
+  // textInput.trigger("focus");
+}); */
+
+
+
+/* ⬇︎ JQUERY TO RECOGNIZE WHEN A BUTTON IS CLICKED AND UPON CLICK EXECUTE THE SPECIFIED ACTION 
+    • In this case the action we are executing is to run the saveInput function
+    • This entire line is what the event parameter in line 56 is referencing */
 $(".saveBtn").on("click", saveInput);
 
+
+/* ⬇︎ SAVE INPUT FUNCTION
+    • Sibilings of each div (aka the timeblocks) are span, textarea, and button. 
+    • The sibilings are an array so that makes: span ➝ 0, textarea ➝ 1, button ➝ 2
+    • Parents refers to the div (aka timeblock) and we only want the immediate parent (go up 1 level in the DOM) so that would be 0
+    • event is click, target is saveBtn */
 function saveInput(event) {
-  //localStorage.setItem("textInput", JSON.stringify());
-  //console.log($(event.target).siblings()[1].value);
   var textVal = $(event.target).siblings()[1].value;
   var timeId = $(event.target).parents()[0].id;
   localStorage.setItem(timeId, textVal);
-
 };
 
+
+/* ⬇︎ LOAD ALL DATA SAVED IN LOCAL STORAGE FUNCTION
+• LINE 68 & 69: Create a for loop to get/retrieve the items (values) saved to each id (key)
+• LINE 69: (i) = the id
+• LINE 70: Append the item (value) retrieved from local storage to it's cordinating id (key) */
 function loadStorage() {
-  for (let i = 8; i <=17; i++) {
+  for (let i = 8; i <= 17; i++) {
     localStorage.getItem(i);
-    console.log(localStorage.getItem(i))
-    $("#" + i).children()[i] = localStorage.getItem(i);
-    //console.log($("#" + i).children()[1]);
-    //console.log(textInput)
     $("#" + i).append(localStorage.getItem(i));
   }
 };
 
 
-
-
 showDate();
 colorCoding();
 loadStorage();
-
-
-
-
-
-
